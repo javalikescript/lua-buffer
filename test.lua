b93095c6b0abbbc505974d81bbeced82afbdba7a
javalikescript <javalikescript@free.fr>
2024-08-11 18:15:33 +0200

    Test len for light user data

--- test.lua
+++ test.lua
@@ -3,28 +3,29 @@ local lu = require('luaunit')
 local bufferLib = require('buffer')
 
 function test_new_size()
-  local b = bufferLib.new(256);
+  local b = bufferLib.new(256)
   lu.assertEquals(bufferLib.len(b), 256)
+  lu.assertEquals(bufferLib.len(bufferLib.lighten(b)), 0)
 end
 
 function test_new_string()
   local s = 'Hello world!'
-  local b = bufferLib.new(s);
+  local b = bufferLib.new(s)
   lu.assertEquals(bufferLib.len(b), string.len(s))
   lu.assertEquals(bufferLib.tostring(b), s)
 end
 
 function test_new_userdata()
   local s = 'Hello world!'
-  local ud = bufferLib.new(s);
-  local b = bufferLib.new(ud);
+  local ud = bufferLib.new(s)
+  local b = bufferLib.new(ud)
   lu.assertEquals(bufferLib.len(b), string.len(s))
   lu.assertEquals(bufferLib.tostring(b), s)
 end
 
 function test_byte()
   local s = 'Hello world!'
-  local b = bufferLib.new(s);
+  local b = bufferLib.new(s)
   lu.assertEquals(bufferLib.byte(b, 1), string.byte(s, 1))
   lu.assertEquals({bufferLib.byte(b, 1, 3)}, {string.byte(s, 1, 3)})
   lu.assertEquals(bufferLib.byte(b), string.byte(s))
@@ -33,58 +34,58 @@ end
 
 function test_byteset()
   local s = 'Hello world!'
-  local b = bufferLib.new(s);
-  bufferLib.byteset(b, 7, bufferLib.byte(b, 7) - 32);
+  local b = bufferLib.new(s)
+  bufferLib.byteset(b, 7, bufferLib.byte(b, 7) - 32)
   lu.assertEquals(bufferLib.tostring(b), 'Hello World!')
 end
 
 function test_byteset_mupltiple()
-  local b = bufferLib.new(3);
-  bufferLib.byteset(b, 1, 65, 66, 67);
+  local b = bufferLib.new(3)
+  bufferLib.byteset(b, 1, 65, 66, 67)
   lu.assertEquals(bufferLib.tostring(b), 'ABC')
 end
 
 function test_memset()
-  local b = bufferLib.new(3);
-  bufferLib.memset(b, 65);
+  local b = bufferLib.new(3)
+  bufferLib.memset(b, 65)
   lu.assertEquals(bufferLib.tostring(b), 'AAA')
-  b = bufferLib.new('---');
-  bufferLib.memset(b, 32);
+  b = bufferLib.new('---')
+  bufferLib.memset(b, 32)
   lu.assertEquals(bufferLib.tostring(b), '   ')
 end
 
 function test_memcpy()
-  local b = bufferLib.new('Hello world!');
-  local c = bufferLib.new('earth');
-  bufferLib.memcpy(b, 7, c);
+  local b = bufferLib.new('Hello world!')
+  local c = bufferLib.new('earth')
+  bufferLib.memcpy(b, 7, c)
   lu.assertEquals(bufferLib.tostring(b), 'Hello earth!')
-  bufferLib.memcpy(b, 7, 'World');
+  bufferLib.memcpy(b, 7, 'World')
   lu.assertEquals(bufferLib.tostring(b), 'Hello World!')
-  bufferLib.memcpy(b, 8, c, 2, 4);
+  bufferLib.memcpy(b, 8, c, 2, 4)
   lu.assertEquals(bufferLib.tostring(b), 'Hello Wartd!')
-  bufferLib.memcpy(b, -6, c);
+  bufferLib.memcpy(b, -6, c)
   lu.assertEquals(bufferLib.tostring(b), 'Hello earth!')
   -- TODO corrected interval values
 end
 
 function test_memcpy_light()
-  local bb = bufferLib.new('Hello world!');
+  local bb = bufferLib.new('Hello world!')
   local l = bufferLib.len(bb)
   local b = bufferLib.lighten(bb)
-  local c = bufferLib.new('earth');
-  bufferLib.memcpy(b, 7, c);
+  local c = bufferLib.new('earth')
+  bufferLib.memcpy(b, 7, c)
   lu.assertEquals(bufferLib.tostring(bb), 'Hello earth!')
-  bufferLib.memcpy(b, 7, 'World');
+  bufferLib.memcpy(b, 7, 'World')
   lu.assertEquals(bufferLib.tostring(bb), 'Hello World!')
-  bufferLib.memcpy(b, 8, c, 2, 4);
+  bufferLib.memcpy(b, 8, c, 2, 4)
   lu.assertEquals(bufferLib.tostring(bb), 'Hello Wartd!')
-  bufferLib.memcpy(b, -6, c);
+  bufferLib.memcpy(b, -6, c)
   lu.assertEquals(bufferLib.tostring(bb), 'Hello Wartd!')
 end
 
 function test_sub()
   local s = 'Hello world!'
-  local b = bufferLib.new(s);
+  local b = bufferLib.new(s)
   lu.assertEquals(bufferLib.sub(b), s)
   lu.assertEquals(bufferLib.sub(b, 1, 4), 'Hell')
   lu.assertEquals(bufferLib.sub(b, 7), 'world!')
@@ -94,7 +95,7 @@ end
 
 function test_sub_light()
   local s = 'Hello world!'
-  local bb = bufferLib.new(s);
+  local bb = bufferLib.new(s)
   local b = bufferLib.lighten(bb)
   lu.assertEquals(bufferLib.sub(b), '')
   lu.assertEquals(bufferLib.sub(b, 1, 4), 'Hell')
