c8dfae374a74ed4fccf546d01039027d46b9836b
javalikescript <javalikescript@free.fr>
2024-08-15 14:38:52 +0200

    Add pointer functions

--- buffer.c
+++ buffer.c
@@ -5,7 +5,6 @@
 /*
 * Lua 5.1 and 5.2 compatibility
 */
-
 #if LUA_VERSION_NUM < 502
 // From Lua 5.3 lauxlib.c
 LUALIB_API void luaL_setfuncs (lua_State *L, const luaL_Reg *l, int nup) {
@@ -26,218 +25,243 @@ LUALIB_API void luaL_setfuncs (lua_State *L, const luaL_Reg *l, int nup) {
 #define lua_isinteger lua_isnumber
 #endif
 
-/*
-Provides functions to deal with userdata as plain byte buffer.
-*/
-static const char *toBuffer(lua_State *l, int i, size_t *len) {
-	if (lua_isstring(l, i)) {
-		return lua_tolstring(l, i, len);
-	} else if (lua_isuserdata(l, i) && !lua_islightuserdata(l, i)) {
-		*len = lua_rawlen(l, i);
-		return (const char *)lua_touserdata(l, i);
-	}
-	return NULL;
+// from lstrlib.c
+// clip result to [1, inf)
+static size_t posrelatI (lua_Integer pos, size_t len) {
+  if (pos > 0)
+    return (size_t)pos;
+  else if (pos == 0)
+    return 1;
+  else if (pos < -(lua_Integer)len)  /* inverted comparison */
+    return 1;  /* clip to 1 */
+  else return len + (size_t)pos + 1;
 }
 
-static int adjustOffset(lua_Integer offset, size_t length) {
-	if (offset < 0) {
-		offset = length + 1 + offset;
-		if (offset < 1) {
-			return 0;
-		}
-	}
-	return offset;
+static size_t unsafeposrelatI(lua_Integer pos, size_t len) {
+  if (len == 0) {
+    if (pos > 0) {
+      return pos;
+    }
+    return 0;
+  }
+  return posrelatI(pos, len);
+}
+
+static size_t getunsafeendpos(lua_State *L, int arg, lua_Integer def, size_t len) {
+  // from lstrlib.c
+  lua_Integer pos = luaL_optinteger(L, arg, def);
+  if (len == 0) {
+    if (pos > 0) {
+      return pos;
+    }
+    return 0;
+  }
+  // clip result to [0, len]
+  if (pos > (lua_Integer)len)
+    return len;
+  else if (pos >= 0)
+    return (size_t)pos;
+  else if (pos < -(lua_Integer)len)
+    return 0;
+  else return len + (size_t)pos + 1;
+}
+
+/*
+Provides functions to deal with userdata as plain byte buffer.
+The indices are corrected following the same rules of function string.sub.
+*/
+
+/*
+Returns a pointer and its associated size or NULL.
+*/
+static const char *toBuffer(lua_State *l, int i, size_t *len) {
+  if (lua_isstring(l, i)) {
+    return lua_tolstring(l, i, len);
+  } else if (lua_isuserdata(l, i) && !lua_islightuserdata(l, i)) {
+    *len = lua_rawlen(l, i);
+    return (const char *)lua_touserdata(l, i);
+  }
+  return NULL;
 }
 
 /*
 Returns a new user data from the specified size, string or user data.
 */
 static int buffer_new(lua_State *l) {
-	size_t nbytes = 0;
-	unsigned char *buffer = NULL;
-	const char *src = NULL;
-	if (lua_isinteger(l, 1)) {
-		nbytes = luaL_checkinteger(l, 1);
-	} else {
-		src = toBuffer(l, 1, &nbytes);
-	}
-	if (nbytes > 0) {
-		buffer = (unsigned char *)lua_newuserdata(l, nbytes);
-		if (src != NULL) {
-			memcpy(buffer, src, nbytes);
-		}
-	} else {
-		lua_pushnil(l);
-	}
-	return 1;
+  size_t nbytes = 0;
+  unsigned char *buffer = NULL;
+  const char *src = NULL;
+  if (lua_isinteger(l, 1)) {
+    nbytes = luaL_checkinteger(l, 1);
+  } else {
+    src = toBuffer(l, 1, &nbytes);
+  }
+  if (nbytes > 0) {
+    buffer = (unsigned char *)lua_newuserdata(l, nbytes);
+    if (src != NULL) {
+      memcpy(buffer, src, nbytes);
+    }
+  } else {
+    lua_pushnil(l);
+  }
+  return 1;
 }
 
 /*
 Returns the length of the specified user data or 0.
 */
 static int buffer_len(lua_State *l) {
-	size_t blen = lua_rawlen(l, 1);
-	lua_pushinteger(l, blen);
-	return 1;
+  size_t blen = lua_rawlen(l, 1);
+  lua_pushinteger(l, blen);
+  return 1;
 }
 
 /*
 Returns the specified user data as a light user data.
 */
 static int buffer_lighten(lua_State *l) {
-	void *p = lua_touserdata(l, 1);
-	lua_pushlightuserdata(l, p);
-	return 1;
+  void *p = lua_touserdata(l, 1);
+  lua_pushlightuserdata(l, p);
+  return 1;
 }
 
 /*
-Returns the specified user data as a string.
+Returns a pointer string representing the specified user data.
 */
-static int buffer_tostring(lua_State *l) {
-	if (lua_isuserdata(l, 1) && !lua_islightuserdata(l, 1)) {
-		size_t blen = lua_rawlen(l, 1);
-		char *buffer = (char *)lua_touserdata(l, 1);
-		lua_pushlstring(l, buffer, blen);
-	} else {
-		lua_pushnil(l);
-	}
+static int buffer_topointer(lua_State *l) {
+  const void *p = lua_touserdata(l, 1);
+   lua_pushlstring(l, (const char *) &p, sizeof(void *));
   return 1;
+}
+
+/*
+Returns a light user data corresponding to the specified pointer string.
+*/
+static int buffer_frompointer(lua_State *l) {
+  size_t len = 0;
+  const char *p = luaL_optlstring(l, 1, NULL, &len);
+  if (len == sizeof(void *)) {
+    lua_pushlightuserdata(l, *((void **) p));
+    return 1;
+  }
+  return 0;
 }
 
 /*
 Returns the bytes.
 */
 static int buffer_byte(lua_State *l) {
-	size_t blen = lua_rawlen(l, 1); // 0 when light
-	unsigned char *buffer = (unsigned char *)lua_touserdata(l, 1);
-	lua_Integer i = adjustOffset(luaL_optinteger(l, 2, 1), blen);
-	lua_Integer j = adjustOffset(luaL_optinteger(l, 3, i), blen);
-	int n, ii;
-	if (i < 1) {
-		i = 1;
-	}
-	if (j > (lua_Integer)blen) {
-		j = blen;
-	}
-	if (i > j) {
-		return 0;
-	}
-	if (j - i >= INT_MAX) {
-		return luaL_error(l, "bytes interval too long");
-	}
-	n = (int)(j -  i) + 1;
-	luaL_checkstack(l, n, "bytes interval too long");
-	for (ii = i - 1; ii < j; ii++) {
-		lua_pushinteger(l, buffer[ii]);
-	}
-	return n;
+  size_t blen = lua_rawlen(l, 1); // 0 when light
+  unsigned char *buffer = (unsigned char *)lua_touserdata(l, 1);
+  size_t i = posrelatI(luaL_optinteger(l, 2, 1), blen);
+  size_t j = getunsafeendpos(l, 3, i, blen);
+  size_t ii;
+  int n = j - i + 1;
+  if (j <= 0 || n <= 0) {
+    return 0;
+  }
+  if (n >= INT_MAX) {
+    return luaL_error(l, "bytes interval too long");
+  }
+  luaL_checkstack(l, n, "bytes interval too long");
+  for (ii = i - 1; ii < j; ii++) {
+    lua_pushinteger(l, buffer[ii]);
+  }
+  return n;
 }
 
 /*
 Sets the bytes in the buffer.
 */
 static int buffer_byteset(lua_State *l) {
-	int n = lua_gettop(l);
-	size_t blen = lua_rawlen(l, 1);
-	unsigned char *buffer = (unsigned char *)lua_touserdata(l, 1);
-	lua_Integer offset = adjustOffset(luaL_optinteger(l, 2, 1), blen);
-	int i;
-	for (i = 3; i <= n; i++) {
-		lua_Integer c = luaL_checkinteger(l, i);
-		unsigned char b = c;
-		luaL_argcheck(l, b == c, i, "value out of range");
-		buffer[offset + i - 4] = b;
-	}
-	return 0;
+  int narg = lua_gettop(l);
+  size_t blen = lua_rawlen(l, 1);
+  unsigned char *buffer = (unsigned char *)lua_touserdata(l, 1);
+  size_t i = unsafeposrelatI(luaL_checkinteger(l, 2), blen);
+  int arg, offset;
+  if (i <= 0 || (blen > 0 && (i + narg - 3 > blen))) {
+    return 0;
+  }
+  offset = i - 1;
+  for (arg = 3; arg <= narg; arg++) {
+    lua_Integer c = luaL_checkinteger(l, arg);
+    unsigned char b = c;
+    luaL_argcheck(l, b == c, arg, "value out of range");
+    buffer[offset] = b;
+    offset++;
+  }
+  return 0;
 }
 
+/*
+Fills the buffer with a specified byte value.
+*/
 static int buffer_memset(lua_State *l) {
-	size_t blen = lua_rawlen(l, 1);
-	unsigned char *buffer = (unsigned char *)lua_touserdata(l, 1);
-	lua_Integer value = luaL_checkinteger(l, 2);
-	unsigned char byteValue = value;
-	luaL_argcheck(l, byteValue == value, 2, "value out of range");
-	memset(buffer, byteValue, blen);
-	return 0;
+  size_t blen = lua_rawlen(l, 1);
+  unsigned char *buffer = (unsigned char *)lua_touserdata(l, 1);
+  lua_Integer value = luaL_optinteger(l, 2, 0);
+  size_t i = posrelatI(luaL_optinteger(l, 3, 1), blen);
+  size_t j = getunsafeendpos(l, 4, -1, blen);
+  unsigned char byteValue = value;
+  luaL_argcheck(l, byteValue == value, 2, "value out of range");
+  if (j > 0 && i <= j) {
+    memset(buffer + i - 1, byteValue, j - i + 1);
+  }
+  return 0;
 }
 
 /*
 Copies the string to the buffer.
 */
 static int buffer_memcpy(lua_State *l) {
-	lua_Integer offset, i, j, ii, n, m;
-	size_t slen, blen;
-	unsigned char *buffer;
-	int islight;
-	const char *src;
-	buffer = (unsigned char *)lua_touserdata(l, 1);
-	islight = lua_islightuserdata(l, 1);
-	blen = lua_rawlen(l, 1);
-	offset = adjustOffset(luaL_optinteger(l, 2, 1), blen);
-	slen = 0;
-	src = toBuffer(l, 3, &slen);
-	i = adjustOffset(luaL_optinteger(l, 4, 1), slen);
-	j = adjustOffset(luaL_optinteger(l, 5, slen), slen);
-	m = blen;
-	if (i < 1) {
-		i = 1;
-	}
-	if ((j > m) && !islight) {
-		j = m;
-	}
-	ii = i - 1;
-	n = j - ii;
-	if ((src != NULL) && (offset >= 1) && (i <= j)) {
-		if (((offset <= m) && (offset + n <= m)) || islight) {
-			memcpy(buffer + offset - 1, src + ii, n);
-		}
-	}
-	return 0;
+  size_t blen = lua_rawlen(l, 1);
+  unsigned char *buffer = (unsigned char *)lua_touserdata(l, 1);
+  size_t slen = 0;
+  const char *src = toBuffer(l, 2, &slen);
+  size_t k = unsafeposrelatI(luaL_optinteger(l, 3, 1), blen);
+  size_t i = posrelatI(luaL_optinteger(l, 4, 1), slen);
+  size_t j = getunsafeendpos(l, 5, -1, slen);
+  if (src != NULL && slen > 0 && k > 0 && j > 0 && i <= j) {
+    memcpy(buffer + k - 1, src + i - 1, j - i + 1);
+  }
+  return 0;
 }
 
 /*
 Returns the specified user data as a string.
 */
 static int buffer_sub(lua_State *l) {
-	size_t blen = lua_rawlen(l, 1);
-	char *buffer = (char *)lua_touserdata(l, 1);
-	lua_Integer i = adjustOffset(luaL_optinteger(l, 2, 1), blen);
-	lua_Integer j = adjustOffset(luaL_optinteger(l, 3, blen), blen);
-	int islight = lua_islightuserdata(l, 1);
-	lua_Integer ii;
-	if (i < 1) {
-		i = 1;
-	}
-	if ((j > (lua_Integer)blen) && !islight) {
-		j = blen;
-	}
-	if (i > j) {
-		lua_pushstring(l, "");
-		return 1;
-	}
-	ii = i - 1;
-	lua_pushlstring(l, buffer + ii, j - ii);
+  size_t blen = lua_rawlen(l, 1);
+  char *buffer = (char *)lua_touserdata(l, 1);
+  size_t i = posrelatI(luaL_optinteger(l, 2, 1), blen);
+  size_t j = getunsafeendpos(l, 3, -1, blen);
+  if (j > 0 && i <= j) {
+    lua_pushlstring(l, buffer + i - 1, j - i + 1);
+  } else {
+    lua_pushstring(l, "");
+  }
   return 1;
 }
 
 LUALIB_API int luaopen_buffer(lua_State *l) {
-	luaL_Reg reg[] = {
-		{ "new", buffer_new },
-		{ "len", buffer_len },
-		{ "lighten", buffer_lighten },
-		{ "tostring", buffer_tostring },
-		{ "byte", buffer_byte },
-		{ "byteset", buffer_byteset },
-		{ "memset", buffer_memset },
-		{ "memcpy", buffer_memcpy },
-		{ "sub", buffer_sub },
-		{ NULL, NULL }
-	};
-	lua_newtable(l);
-	luaL_setfuncs(l, reg, 0);
-	lua_pushliteral(l, "Lua buffer");
-	lua_setfield(l, -2, "_NAME");
-	lua_pushliteral(l, "0.2");
-	lua_setfield(l, -2, "_VERSION");
-	return 1;
+  luaL_Reg reg[] = {
+    { "new", buffer_new },
+    { "len", buffer_len },
+    { "lighten", buffer_lighten },
+    { "topointer", buffer_topointer },
+    { "frompointer", buffer_frompointer },
+    { "byte", buffer_byte },
+    { "byteset", buffer_byteset },
+    { "memset", buffer_memset },
+    { "memcpy", buffer_memcpy },
+    { "sub", buffer_sub },
+    { NULL, NULL }
+  };
+  lua_newtable(l);
+  luaL_setfuncs(l, reg, 0);
+  lua_pushliteral(l, "Lua buffer");
+  lua_setfield(l, -2, "_NAME");
+  lua_pushliteral(l, "0.3");
+  lua_setfield(l, -2, "_VERSION");
+  return 1;
 }
--- test.lua
+++ test.lua
@@ -2,106 +2,147 @@ local lu = require('luaunit')
 
 local bufferLib = require('buffer')
 
-function test_new_size()
+function Test_new_size()
   local b = bufferLib.new(256)
   lu.assertEquals(bufferLib.len(b), 256)
-  lu.assertEquals(bufferLib.len(bufferLib.lighten(b)), 0)
+  lu.assertNil(bufferLib.new())
+  lu.assertNil(bufferLib.new(0))
+  lu.assertNil(bufferLib.new(''))
 end
 
-function test_new_string()
+function Test_new_string()
   local s = 'Hello world!'
   local b = bufferLib.new(s)
   lu.assertEquals(bufferLib.len(b), string.len(s))
-  lu.assertEquals(bufferLib.tostring(b), s)
+  lu.assertEquals(bufferLib.sub(b), s)
+  lu.assertNil(bufferLib.new(''))
 end
 
-function test_new_userdata()
+function Test_new_userdata()
   local s = 'Hello world!'
   local ud = bufferLib.new(s)
   local b = bufferLib.new(ud)
   lu.assertEquals(bufferLib.len(b), string.len(s))
-  lu.assertEquals(bufferLib.tostring(b), s)
+  lu.assertEquals(bufferLib.sub(b), s)
 end
 
-function test_byte()
+function Test_byte()
   local s = 'Hello world!'
   local b = bufferLib.new(s)
   lu.assertEquals(bufferLib.byte(b, 1), string.byte(s, 1))
   lu.assertEquals({bufferLib.byte(b, 1, 3)}, {string.byte(s, 1, 3)})
   lu.assertEquals(bufferLib.byte(b), string.byte(s))
+  lu.assertEquals(table.pack(bufferLib.byte(b)), table.pack(string.byte(s)))
+  lu.assertEquals(table.pack(bufferLib.byte(b, 3, -1)), table.pack(string.byte(s, 3, -1)))
   -- TODO corrected interval values
 end
 
-function test_byteset()
+function Test_byteset()
   local s = 'Hello world!'
   local b = bufferLib.new(s)
+  lu.assertEquals(bufferLib.sub(b), s)
+  bufferLib.byteset(b, 7)
+  lu.assertEquals(bufferLib.sub(b), s)
+  bufferLib.byteset(b, 3, 32, 32, 32)
+  lu.assertEquals(bufferLib.sub(b), 'He    world!')
   bufferLib.byteset(b, 7, bufferLib.byte(b, 7) - 32)
-  lu.assertEquals(bufferLib.tostring(b), 'Hello World!')
+  lu.assertEquals(bufferLib.sub(b), 'He    World!')
 end
 
-function test_byteset_mupltiple()
+function Test_byteset_mupltiple()
   local b = bufferLib.new(3)
   bufferLib.byteset(b, 1, 65, 66, 67)
-  lu.assertEquals(bufferLib.tostring(b), 'ABC')
+  lu.assertEquals(bufferLib.sub(b), 'ABC')
 end
 
-function test_memset()
+function Test_memset()
   local b = bufferLib.new(3)
   bufferLib.memset(b, 65)
-  lu.assertEquals(bufferLib.tostring(b), 'AAA')
+  lu.assertEquals(bufferLib.sub(b), 'AAA')
   b = bufferLib.new('---')
   bufferLib.memset(b, 32)
-  lu.assertEquals(bufferLib.tostring(b), '   ')
+  lu.assertEquals(bufferLib.sub(b), '   ')
 end
 
-function test_memcpy()
+local function checkMemCopy(b, c)
+  bufferLib.memcpy(b, c, 7)
+  lu.assertEquals(bufferLib.sub(b), 'Hello earth!')
+  bufferLib.memcpy(b, 'World', 7)
+  lu.assertEquals(bufferLib.sub(b), 'Hello World!')
+  bufferLib.memcpy(b, c, 8, 2, 4)
+  lu.assertEquals(bufferLib.sub(b), 'Hello Wartd!')
+  bufferLib.memcpy(b, c, -6)
+  lu.assertEquals(bufferLib.sub(b), 'Hello earth!')
+  bufferLib.memcpy(b, 'Hi   ')
+  lu.assertEquals(bufferLib.sub(b), 'Hi    earth!')
+end
+
+function Test_memcpy()
   local b = bufferLib.new('Hello world!')
   local c = bufferLib.new('earth')
-  bufferLib.memcpy(b, 7, c)
-  lu.assertEquals(bufferLib.tostring(b), 'Hello earth!')
-  bufferLib.memcpy(b, 7, 'World')
-  lu.assertEquals(bufferLib.tostring(b), 'Hello World!')
-  bufferLib.memcpy(b, 8, c, 2, 4)
-  lu.assertEquals(bufferLib.tostring(b), 'Hello Wartd!')
-  bufferLib.memcpy(b, -6, c)
-  lu.assertEquals(bufferLib.tostring(b), 'Hello earth!')
-  -- TODO corrected interval values
+  checkMemCopy(b, c)
 end
 
-function test_memcpy_light()
+function Test_memcpy_string()
+  local b = bufferLib.new('Hello world!')
+  checkMemCopy(b, 'earth')
+end
+
+function Test_memcpy_light()
   local bb = bufferLib.new('Hello world!')
   local l = bufferLib.len(bb)
   local b = bufferLib.lighten(bb)
   local c = bufferLib.new('earth')
-  bufferLib.memcpy(b, 7, c)
-  lu.assertEquals(bufferLib.tostring(bb), 'Hello earth!')
-  bufferLib.memcpy(b, 7, 'World')
-  lu.assertEquals(bufferLib.tostring(bb), 'Hello World!')
-  bufferLib.memcpy(b, 8, c, 2, 4)
-  lu.assertEquals(bufferLib.tostring(bb), 'Hello Wartd!')
-  bufferLib.memcpy(b, -6, c)
-  lu.assertEquals(bufferLib.tostring(bb), 'Hello Wartd!')
+  bufferLib.memcpy(b, c, 7)
+  lu.assertEquals(bufferLib.sub(bb), 'Hello earth!')
+  bufferLib.memcpy(b, 'World', 7)
+  lu.assertEquals(bufferLib.sub(bb), 'Hello World!')
+  bufferLib.memcpy(b, c, 8, 2, 4)
+  lu.assertEquals(bufferLib.sub(bb), 'Hello Wartd!')
+  bufferLib.memcpy(b, c, -6)
+  lu.assertEquals(bufferLib.sub(bb), 'Hello Wartd!')
+  bufferLib.memcpy(b, 'Hi   ')
+  lu.assertEquals(bufferLib.sub(bb), 'Hi    Wartd!')
 end
 
-function test_sub()
-  local s = 'Hello world!'
-  local b = bufferLib.new(s)
-  lu.assertEquals(bufferLib.sub(b), s)
-  lu.assertEquals(bufferLib.sub(b, 1, 4), 'Hell')
-  lu.assertEquals(bufferLib.sub(b, 7), 'world!')
-  lu.assertEquals(bufferLib.sub(b, 7, -2), 'world')
+local function checkSub(b)
+  lu.assertEquals(bufferLib.sub(b, 1, 5), 'Hello')
+  lu.assertEquals(bufferLib.sub(b, 7, 11), 'world')
   lu.assertEquals(bufferLib.sub(b, 2, 1), '')
 end
 
-function test_sub_light()
-  local s = 'Hello world!'
-  local bb = bufferLib.new(s)
-  local b = bufferLib.lighten(bb)
+local function checkSubLight(b)
+  checkSub(b)
   lu.assertEquals(bufferLib.sub(b), '')
-  lu.assertEquals(bufferLib.sub(b, 1, 4), 'Hell')
   lu.assertEquals(bufferLib.sub(b, 7), '')
   lu.assertEquals(bufferLib.sub(b, 7, -2), '')
-  lu.assertEquals(bufferLib.sub(b, 2, 1), '')
+end
+
+function Test_sub()
+  local s = 'Hello world!'
+  local b = bufferLib.new(s)
+  checkSub(b)
+  lu.assertEquals(bufferLib.sub(b), s)
+  lu.assertEquals(bufferLib.sub(b, 7), 'world!')
+  lu.assertEquals(bufferLib.sub(b, 7, -2), 'world')
+end
+
+function Test_sub_light()
+  local s = 'Hello world!'
+  local b = bufferLib.new(s)
+  local l = bufferLib.lighten(b)
+  checkSubLight(l)
+end
+
+function Test_sub_pointer()
+  local b = bufferLib.new('Hi')
+  local l = bufferLib.lighten(b)
+  local p = bufferLib.topointer(b)
+  local lp = bufferLib.frompointer(p)
+  lu.assertEquals(type(l), 'userdata')
+  lu.assertEquals(type(p), 'string')
+  lu.assertEquals(type(lp), 'userdata')
+  lu.assertEquals(l, lp)
 end
 
 os.exit(lu.LuaUnit.run())
